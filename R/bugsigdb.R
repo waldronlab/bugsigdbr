1a8572fd117bb1a61482bf35e91b6152a36b1327
lgeistlinger <ludwig.geistlinger@gmail.com>
2021-04-02 13:25:11 -0400

    add importBugSigDB function

---
+++ R/bugsigdb.R
@@ -0,0 +1,37 @@
+#' Obtain published microbial signatures from bugsigdb.org
+#' @param cache logical. Should a locally cached version used if available?
+#' Defaults to \code{TRUE}.
+#' @return a \code{\link{data.frame}}.
+#' @references BugSigDB: \url{https://bugsigdb.org}
+#' @examples
+#'
+#'      res <- importBugSigDB()
+#'
+#' @export
+importBugSigDB <- function(cache = TRUE)
+{
+    rname <- "bugsigdb"
+
+    # should a cache version be used?        
+    if(cache)
+    {
+        bugsigdb <- .getResourceFromCache(rname)
+        if(!is.null(bugsigdb)) return(bugsigdb) 
+    }
+     
+    # pull the data
+    sigs <- readr::read_csv("https://tinyurl.com/yakgsowm")
+    exps <- readr::read_csv("https://tinyurl.com/yb2fmpa3")
+    studs <- readr::read_csv("https://tinyurl.com/ycg8fs9x")
+
+    # merge the data
+    ind <- colnames(exps) == "Experiment page name"
+    colnames(exps)[ind] <- "Experiment"
+    ind <- colnames(studs) == "Study page name"
+    colnames(studs)[ind] <- "Study"
+    sig.exp <- plyr::join(exps, sigs, by = c("Study", "Experiment"))
+    bugsigdb <- plyr::join(studs, sig.exp, by = "Study")
+
+    .cacheResource(bugsigdb, rname)
+    return(bugsigdb)
+}
