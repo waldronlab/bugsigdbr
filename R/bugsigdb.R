d833c70d600816ea79b80dc795b61c992c29045c
lgeistlinger <ludwig.geistlinger@gmail.com>
2023-05-20 15:00:02 -0400

    support full SHA, doc enhancements, empty taxon fix

--- R/bugsigdb.R
+++ R/bugsigdb.R
@@ -1,7 +1,36 @@
 #' Obtain published microbial signatures from bugsigdb.org
+#'
+#' @details There are three different options to obtain data from 
+#' BugSigDB, as determined by the \code{version} argument.
+#' \itemize{
+#' \item a Zenodo DOI: use this option if you would like to obtain
+#' one of the stable release versions of BugSigDB on Zenodo. These
+#' stable release versions of BugSigDB have been automatically checked and
+#' manually reviewed and provide for the highest data quality. Select this option
+#' if you would like to incorporate BugSigDB into analysis and published
+#' research. If not specified otherwise, the \code{importBugSigDB} function
+#' will obtain the most recent stable release from Zenodo by default.
+#' \item \code{"devel"}: use this option to obtain the latest version
+#' ("bleeding edge") of BugSigDB from the BugSigDBExports GitHub repo 
+#' (see references).
+#' Note that this will also include incomplete and not reviewed content,
+#' which should be filtered out prior to an analysis.
+#' Select this option if you are a curator that actively contributes to
+#' BugSigDB and would like to access data that you and other curators have 
+#' recently contributed to BugSigDB and that has not been included in a stable
+#' release yet. 
+#' \item a git commit hash: it might be occasionally of interest to obtain
+#' a specific snapshot of the BugSigDBExports GitHub repo, e.g. for the sake of 
+#' debugging and troubleshooting. This can be done by providing the short
+#' 7-character git commit hash (SHA) or the full SHA of the export of choice.
+#' To provide the full SHA, go to the BugSigDBExports commits page (see references)
+#' and use the copy symbol to the left of the 7-character codes to copy the full
+#' SHA code of the export version you want to use. 
+#'}
+#'
 #' @param version character. A Zenodo DOI, git commit hash, or "devel".
 #' Defaults to the most recent stable release on Zenodo, which includes
-#' complete and reviewed content from BugSigDB.
+#' complete and reviewed content from BugSigDB. See details.
 #' @param cache logical. Should a locally cached version used if available?
 #' Defaults to \code{TRUE}.
 #'
@@ -12,6 +41,9 @@
 #'
 #' Latest version (incl. not reviewed content): 
 #'      \url{https://github.com/waldronlab/BugSigDBExports}
+#'
+#' BugSigDBExports commits page:
+#'      \url{https://github.com/waldronlab/BugSigDBExports/commits/main}
 #' @examples
 #'
 #'  df <- importBugSigDB()
@@ -26,14 +58,15 @@ importBugSigDB <- function(version = "10.5281/zenodo.6468009", cache = TRUE)
         version <- sub("^10.5281/zenodo\\.", "", version)
         url <- paste0("https://zenodo.org/record/", version, "/files/full_dump.csv")
     } 
-    else if(version == "devel" || grepl("^[0-9a-z]{7}$", version))
+    else if(version == "devel" || grepl("^[0-9a-z]{7}", version))
     {
-        if(version == "devel") version = "main"
+        if(version == "devel") version <- "main"
+        else version <- substring(version, 1, 7) 
         url <- paste0("https://raw.githubusercontent.com/waldronlab/",
                       "BugSigDBExports/", version, "/full_dump.csv")
     } 
     else stop("Version ", version, " does not exist. The version must be a",
-              " DOI, such as '10.5281/zenodo.5819260', a Github hash,",
+              " DOI such as '10.5281/zenodo.5819260', a Github hash",
               " such as '30383a9', or 'devel'.")
 
     # should a cache version be used?
@@ -57,6 +90,11 @@ importBugSigDB <- function(version = "10.5281/zenodo.6468009", cache = TRUE)
     dat <- as.data.frame(dat)
     dat[["MetaPhlAn taxon names"]] <- strsplit(dat[["MetaPhlAn taxon names"]], ",")
     dat[["NCBI Taxonomy IDs"]] <- strsplit(dat[["NCBI Taxonomy IDs"]], ";")
+
+    .rmEmpty <- function(x) x[x != ""] 
+    dat[["MetaPhlAn taxon names"]] <- lapply(dat[["MetaPhlAn taxon names"]], .rmEmpty)
+    dat[["NCBI Taxonomy IDs"]] <- lapply(dat[["NCBI Taxonomy IDs"]], .rmEmpty)
+
     saveRDS(dat, file = to)
     return(TRUE)
 }
