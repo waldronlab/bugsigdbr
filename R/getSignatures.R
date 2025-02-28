4fdc880a8cac6509572448d4739d62f2d0b70522
J Wokaty <jwokaty@users.noreply.github.com>
2025-02-27 21:09:55 -0500

    Fix getSignatures after Study ID uses PMID

--- R/getSignatures.R
+++ R/getSignatures.R
@@ -53,7 +53,7 @@ getSignatures <- function(df,
     df <- df[nna,]
 
     # extract signatures
-    is.study <- grepl("^Study [0-9]+$", df[["Study"]])
+    is.study <- grepl("^(Study )?[0-9]+$", df[["Study"]])
     is.exp <- grepl("^Experiment [0-9]+$", df[["Experiment"]])
     df <- df[is.study & is.exp, ]
     
