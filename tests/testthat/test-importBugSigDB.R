0c3fd40dfc355bd313daf234e8d9c68366f3b442
J Wokaty <jwokaty@users.noreply.github.com>
2025-03-03 19:53:19 -0500

    Test that Study, Condition are not NA in devel

--- tests/testthat/test-importBugSigDB.R
+++ tests/testthat/test-importBugSigDB.R
@@ -27,6 +27,8 @@ test_that("importBugSigDB from the edge (devel)", {
     bsdb <- bugsigdbr::importBugSigDB(version = "devel", cache = FALSE)
     url <- "https://tinyurl.com/3nvzm3fx"
     checkImport(bsdb, url, nrows = 5450)
+    expect_true(all(!is.na(bsdb$Study)))
+    expect_true(all(!is.na(bsdb$Condition)))
 })
 
 test_that("importBugSigDB from github hash", {
